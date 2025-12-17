4b2a2bde27f673c08e3c5da37dce7b171447e4a4
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-12-17 14:36:02 -0800

    Create auth-controller.js

---
+++ back-end/controllers/auth-controller.js
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt";
+import UserModel from "../models/user-model";
+import AppError from "../utilites/app_error";
+import { trycatchHandler } from "../utilites/trycatch_handler";
+
+export const register = trycatchHandler(async (req, res) => {
+  const { name, phone, password } = req.body;
+
+  const exists = await UserModel.getUserPhone(phone);
+  if (exists.length) {
+    throw new AppError(
+      109,
+      "این شماره تلفن قبلاً ثبت نام کرده است",
+      409
+    );
+  }
+
+  const hashPassword = await bcrypt.hash(password, 12);
+
+  await UserModel.addUser(name, phone, hashPassword);
+
+  res.status(201).json({
+    success: true,
+    message: "ثبت نام با موفقیت انجام شد",
+  });
+});
