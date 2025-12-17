03797e34e2443351d7d4d5630739894241780256
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-12-17 14:36:49 -0800

    Create auth.schema.js

---
+++ back-end/validators/auth.schema.js
@@ -0,0 +1,31 @@
+import Joi from "joi";
+
+export const registerSchema = Joi.object({
+  name: Joi.string()
+    .trim()
+    .min(3)
+    .max(50)
+    .pattern(/^[\u0600-\u06FFa-zA-Z\s]+$/)
+    .required()
+    .messages({
+      "string.pattern.base": "نام فقط می‌تواند شامل حروف باشد",
+    }),
+
+  phone: Joi.string()
+    .trim()
+    .pattern(/^09\d{9}$/)
+    .required()
+    .messages({
+      "string.pattern.base": "شماره تلفن معتبر نیست",
+    }),
+
+  password: Joi.string()
+    .min(8)
+    .max(64)
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/)
+    .required()
+    .messages({
+      "string.pattern.base":
+        "رمز عبور باید شامل حرف بزرگ، حرف کوچک، عدد و کاراکتر خاص باشد",
+    }),
+});
