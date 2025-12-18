f920754e6672259a84f054de5eab5bd4827ed462
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-12-17 23:22:11 -0800

    Update auth-controller.js

--- back-end/controllers/auth-controller.js
+++ back-end/controllers/auth-controller.js
@@ -8,11 +8,7 @@ export const register = trycatchHandler(async (req, res) => {
 
   const exists = await UserModel.getUserPhone(phone);
   if (exists.length) {
-    throw new AppError(
-      109,
-      "این شماره تلفن قبلاً ثبت نام کرده است",
-      409
-    );
+    throw new AppError(109, "این شماره تلفن قبلاً ثبت نام کرده است", 409);
   }
 
   const hashPassword = await bcrypt.hash(password, 12);
@@ -23,4 +19,19 @@ export const register = trycatchHandler(async (req, res) => {
     success: true,
     message: "ثبت نام با موفقیت انجام شد",
   });
+});
+
+export const login = trycatchHandler(async (req, res) => {
+  const { phone, password } = req.body;
+  const user = await UserModel.getUserPhone(phone);
+  if (!user.length) {
+    throw new AppError(101, "شماره تلفن یا رمز عبور اشتباه است", 401);
+  }
+  const validPassword = await bcrypt.compare(password, user[0].password);
+  if (!validPassword)
+    throw new AppError(101, "شماره تلفن یا رمز عبور اشتباه است", 401);
+
+  res.status(200).json({
+    message: "ورود با موفقیت"
+  })
 });
