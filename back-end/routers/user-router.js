1145e0676777722f904b7dba99d034890bb70b2d
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-12-17 23:22:17 -0800

    Update user-router.js

--- back-end/routers/user-router.js
+++ back-end/routers/user-router.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { registerSchema } from "../validators/auth.schema.js";
-import { register } from "../controllers/auth-controller.js";
+import { loginSchema, registerSchema } from "../validators/auth.schema.js";
+import { login, register } from "../controllers/auth-controller.js";
 import { validate } from "../middlware/validate.js";
-import { registerLimiter } from "../middlware/rate-limit.js";
-
+import { LoginLimiter, registerLimiter } from "../middlware/rate-limit.js";
 const router = express.Router();
 
 router.post("/register", registerLimiter, validate(registerSchema), register);
+router.post("/login", LoginLimiter, validate(loginSchema), login);
 
 export default router;
