cf5b363a108acd72c273a8c6f82cef062466a276
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-12-18 16:04:49 -0800

    Update user-router.js

--- back-end/routers/user-router.js
+++ back-end/routers/user-router.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { loginSchema, registerSchema } from "../validators/auth.schema.js";
-import { login, register } from "../controllers/auth-controller.js";
+import { loginSchema, otpSchema, registerSchema } from "../validators/auth.schema.js";
+import { login, otpCheck, register } from "../controllers/auth-controller.js";
 import { validate } from "../middlware/validate.js";
-import { LoginLimiter, registerLimiter } from "../middlware/rate-limit.js";
+import { LoginLimiter, otpLimiter, registerLimiter } from "../middlware/rate-limit.js";
 const router = express.Router();
 
 router.post("/register", registerLimiter, validate(registerSchema), register);
 router.post("/login", LoginLimiter, validate(loginSchema), login);
+router.post("/codeOtp" , otpLimiter , validate(otpSchema) , otpCheck)
 
 export default router;
