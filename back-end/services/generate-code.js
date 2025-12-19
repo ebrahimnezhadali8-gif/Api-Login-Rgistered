08dac8963314ae2ad68ab57db25bc2a0ecd97ef5
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-12-18 16:04:53 -0800

    Create generate-code.js

---
+++ back-end/services/generate-code.js
@@ -0,0 +1,10 @@
+import crypto from "crypto";
+//generate code with nummber and alefba
+export function generateCode() {
+  return crypto.randomInt(100000, 999999).toString();
+}
+
+//hash
+export function hashCode(key) {
+  return crypto.createHash("sha256").update(key).digest("hex");
+}
