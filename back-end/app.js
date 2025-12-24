5ae778236139725f73b8f2abc62450339b49bb2b
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-12-24 00:44:03 -0800

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -3,10 +3,13 @@ import express from "express";
 const app = express();
 import userRouter from "./routers/user-router.js";
 import errorHandler from "./middlware/error_handler.js";
+import reqContext from "./middlware/req-context.js";
+import { httpLogger } from "./middlware/http-log.js";
 
 app.use(cors());
 app.use(express.json());
-
+app.use(reqContext);
+app.use(httpLogger);
 app.get("/", (req, res) => {
   res.send("Login API is running...");
 });
