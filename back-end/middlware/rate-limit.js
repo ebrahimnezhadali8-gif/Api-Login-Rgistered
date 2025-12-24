0f7cf3f2b7eb58591500979152da952e9cf1c305
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-12-24 00:44:46 -0800

    Update rate-limit.js

--- back-end/middlware/rate-limit.js
+++ back-end/middlware/rate-limit.js
@@ -1,32 +1,74 @@
 import rateLimit from "express-rate-limit";
 
+// all limiters
+const baseLimiter = {
+  standardHeaders: true,
+  legacyHeaders: false,
+};
+
+// Register limiter
 export const registerLimiter = rateLimit({
+  ...baseLimiter,
   windowMs: 15 * 60 * 1000,
   max: 10,
-  standardHeaders: true,
-  legacyHeaders: false,
   message: {
     status: 429,
     message: "تعداد تلاش بیش از حد مجاز است، بعداً دوباره امتحان کنید",
   },
 });
 
-export const LoginLimiter = rateLimit({
+// Login limiter
+export const loginLimiter = rateLimit({
+  ...baseLimiter,
   windowMs: 2 * 60 * 1000,
   max: 7,
-  standardHeaders: true,
-  legacyHeaders: false,
   message: {
     status: 429,
     message: "تعداد تلاش بیش از حد مجاز است، بعداً دوباره امتحان کنید",
   },
 });
 
+// Limited by phone number (fallback to IP)
 export const otpLimiter = rateLimit({
+  ...baseLimiter,
   windowMs: 2 * 60 * 1000,
   max: 3,
-  standardHeaders: true,
-  legacyHeaders: false,
+  keyGenerator: (req) => req.body.phone || req.ip,
+  message: {
+    status: 429,
+    message: "تعداد تلاش بیش از حد مجاز است، بعداً دوباره امتحان کنید",
+  },
+});
+
+// Prevents OTP spam
+export const otpResendLimiter = rateLimit({
+  ...baseLimiter,
+  windowMs: 2 * 60 * 1000,
+  max: 3,
+  keyGenerator: (req) => req.body.phone || req.ip,
+  message: {
+    status: 429,
+    message: "تعداد تلاش بیش از حد مجاز است، بعداً دوباره امتحان کنید",
+  },
+});
+
+// Forgot password OTP limiter
+export const otpForgotLimiter = rateLimit({
+  ...baseLimiter,
+  windowMs: 2 * 60 * 1000,
+  max: 3,
+  keyGenerator: (req) => req.body.phone || req.ip,
+  message: {
+    status: 429,
+    message: "تعداد تلاش بیش از حد مجاز است، بعداً دوباره امتحان کنید",
+  },
+});
+
+// Change password limiter
+export const changePasswordLimiter = rateLimit({
+  ...baseLimiter,
+  windowMs: 15 * 60 * 1000,
+  max: 10,
   message: {
     status: 429,
     message: "تعداد تلاش بیش از حد مجاز است، بعداً دوباره امتحان کنید",
