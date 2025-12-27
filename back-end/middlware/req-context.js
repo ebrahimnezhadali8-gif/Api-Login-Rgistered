4350791259e049a10f0dee3cb2bb8feab5525559
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-12-27 10:45:52 -0800

    Update req-context.js

--- back-end/middlware/req-context.js
+++ back-end/middlware/req-context.js
@@ -1,6 +1,6 @@
 import { v4 as uuid } from "uuid";
 
 export default function reqContext(req, res, next) {
-  req.requestId = req.headers["x-request-id"] || uuid(); // اگر کلاینت requestId داد استفاده کن
+  req.requestId = req.headers["x-request-id"] || uuid(); // req header front or generate id uuid
   next();
 }
