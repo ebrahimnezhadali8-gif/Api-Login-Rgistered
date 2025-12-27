3d808966c158b784a182d43ae9479586fc6d5f90
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-12-27 10:45:32 -0800

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -5,9 +5,21 @@ import userRouter from "./routers/user-router.js";
 import errorHandler from "./middlware/error_handler.js";
 import reqContext from "./middlware/req-context.js";
 import { httpLogger } from "./middlware/http-log.js";
+import helmet from "helmet";
 
+app.set("trust proxy", true);
 app.use(cors());
 app.use(express.json());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+      },
+    },
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+  })
+);
 app.use(reqContext);
 app.use(httpLogger);
 app.get("/", (req, res) => {
