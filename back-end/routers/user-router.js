548eb29633b604d9f1ab540f9a7d01544bc7d834
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2025-12-29 17:45:41 -0800

    Update user-router.js

--- back-end/routers/user-router.js
+++ back-end/routers/user-router.js
@@ -1,40 +1,8 @@
 import express from "express";
-import {
-  changePasswordSchema,
-  loginSchema,
-  otpSchema,
-  registerSchema,
-  resendCode,
-} from "../validators/auth.schema.js";
-import {
-  login,
-  otpCheckForgot,
-  otpCheckLogin,
-  register,
-  sendCodeOtp,
-  updatePassword,
-} from "../controllers/auth-controller.js";
-import { validate } from "../middlware/validate.js";
-import {
-  changePasswordLimiter,
-  loginLimiter,
-  otpForgotLimiter,
-  otpLimiter,
-  otpResendLimiter,
-  registerLimiter,
-} from "../middlware/rate-limit.js";
+import { auth } from "../middlware/auth.js";
+import { getUserMe } from "../controllers/user-controller.js";
 const router = express.Router();
 
-router.post("/register", registerLimiter, validate(registerSchema), register);
-router.post("/login", loginLimiter, validate(loginSchema), login);
-router.post("/codeOtp", otpLimiter, validate(otpSchema), otpCheckLogin);
-router.post("/resendCode", otpResendLimiter, validate(resendCode), sendCodeOtp);
-router.post("/forgotOtp", otpForgotLimiter, validate(otpSchema), otpCheckForgot);
-router.post(
-  "/changePassword",
-  changePasswordLimiter,
-  validate(changePasswordSchema),
-  updatePassword
-);
+router.get("/me", auth, getUserMe);
 
 export default router;
